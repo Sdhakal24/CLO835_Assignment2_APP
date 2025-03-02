de11b81cd94a8d258a036bb3d5d694d2b69a0369
Sdhakal24 <sandesh841maple@gmail.com>
2025-03-01 20:30:33 -0500

    s3 bucket naming and configuration file

--- aws_architecture/terraform/network/config.tf
+++ aws_architecture/terraform/network/config.tf
@@ -1,6 +1,7 @@
+#s3 bucket naming and configuration file 
 terraform {
   backend "s3" {
-    bucket = "clo835-assignment01"                         //  giving S3 bucket name  to save terraform state
+    bucket = "clo835-assignment03"                         //  giving S3 bucket name  to save terraform state
     key    = "terraform/network/terraform.tfstate"        // Object name and pathway
     region = "us-east-1"                                 // Region of availability zone 
   }
