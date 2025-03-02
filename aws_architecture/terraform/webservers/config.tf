32764f7df20599a91dfa08c1b416ba5fdc45c779
Sdhakal24 <sandesh841maple@gmail.com>
2025-03-01 20:28:18 -0500

    updated the s3 bucket name in configuration

--- aws_architecture/terraform/webservers/config.tf
+++ aws_architecture/terraform/webservers/config.tf
@@ -1,6 +1,7 @@
+# Providing the S3 bucket details
 terraform {
   backend "s3" {
-    bucket = "clo835-assignment01"                             // giving the S3 bucket name  to save terraform state
+    bucket = "clo835-assignment02"                             // giving the S3 bucket name  to save terraform state
     key    = "terraform/webserver/terraform.tfstate"          // Object name 
     region = "us-east-1"                                     // Region of availability zone 
   } 
