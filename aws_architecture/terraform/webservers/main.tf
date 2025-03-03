076a73d36c85ba98ffece42990782dd09063ad6d
Sdhakal24 <sandesh841maple@gmail.com>
2025-03-02 21:47:12 -0500

    added some feature

--- aws_architecture/terraform/webservers/main.tf
+++ aws_architecture/terraform/webservers/main.tf
@@ -41,6 +41,11 @@ resource "aws_instance" "web_server" {
   security_groups             = [aws_security_group.web_server_sg.id]
   associate_public_ip_address = true
 
+  root_block_device {
+      volume_size = 64
+      volume_type = "gp2"
+    }
+
   tags = merge(var.default_tags, {
     "Name" = "${var.prefix}-WebServer"
   })
